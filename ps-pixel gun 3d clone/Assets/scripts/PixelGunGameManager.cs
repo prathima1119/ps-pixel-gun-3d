fa962cffdb145b36030fa2ebc30ad2e2de9b9922
prathima <rameshchigullapally977@gmail.com>
2019-07-17 11:27:41 -0700

    player instantiation

--- ps-pixel gun 3d clone/Assets/scripts/PixelGunGameManager.cs
+++ ps-pixel gun 3d clone/Assets/scripts/PixelGunGameManager.cs
@@ -7,10 +7,20 @@ using Photon.Realtime;
 public class PixelGunGameManager : MonoBehaviourPunCallbacks
 
 {
+    [SerializeField]
+    GameObject playerPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+      if(PhotonNetwork.IsConnected)
+        {
+            if(playerPrefab!=null)
+            {
+                int randomPoint = Random.Range(-20, 20);
+                PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(randomPoint, 0, randomPoint), Quaternion.identity);
+            }
+           
+        }
     }
 
     // Update is called once per frame
